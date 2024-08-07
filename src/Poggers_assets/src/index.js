11527f020dfee2f4f68298844915bdf3433405ca
root <root@Kshitij>
2024-08-08 01:36:03 +0530

    Initial commit.

---
+++ src/Poggers_assets/src/index.js
@@ -0,0 +1,19 @@
+import { Poggers } from "../../declarations/Poggers";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await Poggers.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
